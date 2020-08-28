049c17fb10140ac07f3c721d875866b4e08e468e
stamic1 <bhatgaurav96@gmail.com>
2020-08-28 10:01:29 +0530

    creating feature miodule and componment for it lazy loaded module

--- product-mart/src/app/app.module.ts
+++ product-mart/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { ProductsModule } from "./products/products.module"
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     PmMaterialModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
