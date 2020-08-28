98536a3efa813fcf0805c2c22553a151da0fd5e2
stamic1 <bhatgaurav96@gmail.com>
2020-08-28 09:32:12 +0530

    home component added

--- product-mart/src/app/app.module.ts
+++ product-mart/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { PmMaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
